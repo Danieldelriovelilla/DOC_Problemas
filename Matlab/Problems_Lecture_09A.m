ba1cec36aa33fa5859fc8279f9dd63b2601be808
Danieldelriovelilla <daniel_drv@hotmail.es>
2022-01-13 18:19:09 +0100

    corregir elipsoides

--- Matlab/Problems_Lecture_09A.m
+++ Matlab/Problems_Lecture_09A.m
@@ -71,28 +71,47 @@ disp(" "); disp("%   ---   ---   %"); disp(" ")
 
 %% EJERCICIO 3
 % Fantasia
-w = 1*[1.5, 1.5, 1]';
+w = 1*[1, 1, 1]';
 I1 = 2;
 I2 = 2;
 I3 = 1;
 I = doc.Inertia_Matrix(I1, I2, I3);
-
-% Kinetic energy elipsoid
 T = doc.Kinetic_Energy(I, w);
-ke = w.^2./(2*diag(I));
-
-% Angular momentum elipsoid
 h = doc.Angular_Momentum_Iw(I, w);
-he = w.^2./(2*h);
+
+% Elipsoide en ejes w
+wT = sqrt( 2*T./diag(I) );
+wh = h./diag(I);
 
 h = figure();
     hold on
-    [xe, ye, ze] = ellipsoid(0, 0, 0, ke(1), ke(2), ke(3));
-    [xh, yh, zh] = ellipsoid(0, 0, 0, he(1), he(2), he(3));
+    [xe, ye, ze] = ellipsoid(0, 0, 0, wT(1), wT(2), wT(3));
+    [xh, yh, zh] = ellipsoid(0, 0, 0, wh(1), wh(2), wh(3));
     surf(xe, ye, ze,'FaceColor',[0 0.4470 0.7410], 'FaceAlpha', 0.5, 'EdgeColor','none')
     surf(xh, yh, zh, 'FaceColor',[0.8500 0.3250 0.0980],'FaceAlpha', 0.75, 'EdgeColor','none')
     view(-30, 20)
+    legend("w", "h")
+    xlabel('w1'); ylabel('w2'); zlabel('w3')
     grid on; box on; axis equal
+    title("Elipsoide en ejes \omega")
+
+% Angular momentum elipsoid
+hT = sqrt( 2*diag(I)*T );
+hh = ones(3,1);
+
+h = figure();
+    hold on
+    [xe, ye, ze] = ellipsoid(0, 0, 0, hT(1), hT(2), hT(3));
+    [xh, yh, zh] = ellipsoid(0, 0, 0, hh(1), hh(2), hh(3));
+    surf(xe, ye, ze,'FaceColor',[0 0.4470 0.7410], 'FaceAlpha', 0.5, 'EdgeColor','none')
+    surf(xh, yh, zh, 'FaceColor',[0.8500 0.3250 0.0980],'FaceAlpha', 0.75, 'EdgeColor','none')
+    view(-30, 20)
+    legend("w", "h")
+    xlabel('h1'); ylabel('h2'); zlabel('h3')
+    grid on; box on; axis equal
+    title("Elipsoide en ejes h")
+
+
 
 
     
@@ -101,7 +120,6 @@ disp("EJERCICIO 2")
 % Integrar ecuaciones dinamica - cinematica de actitud
 
 % Variables independientes
-Is = sym('Is',[3 1]);
 th = sym('th',[3 1]);
 dth = sym('dth',[3 1]);
 w = sym('w',[3 1]);
@@ -115,9 +133,9 @@ w0 = [0.05, 0.02, -0.02]';
 
 % CINEMATICA
 % Transformacion velocidad angular
-w21 = dth(1)*[1, 0, 0]' +...                       % Ultimo eje de giro - 1
-    dth(2)*doc.C1(th(1))*[0, 1, 0]' +...              % Segundo eje de giro - 2
-    dth(3)*doc.C1(th(1))*doc.C2(th(2))*[0, 0, 1]';       % Tercer eje de giro - 3
+w21 = dth(1)*[1, 0, 0]' +...                            % Ultimo eje de giro - 1
+    dth(2)*doc.C1(th(1))*[0, 1, 0]' +...                % Segundo eje de giro - 2
+    dth(3)*doc.C1(th(1))*doc.C2(th(2))*[0, 0, 1]';      % Tercer eje de giro - 3
 
 % Relacionar A*dw = w
 eq_dth_th = w21 == w;
@@ -129,27 +147,27 @@ eq_th_dth = dth == A\w;
 
 
 % DINAMICA
-I = doc.Inertia_Matrix(Is(1), Is(2), Is(3));
+% Inercia
+I = doc.Inertia_Matrix(210, 200, 118);
+
+% Ecuacion de euler
 eq_dw_w = doc.Euler_Equation(I, w, dw, To);
 
-% Subs the data given on the text
-eq_dw_w = subs(eq_dw_w, To, [0;0;0]);
-eq_dw_w = subs(eq_dw_w, Is, [210;200;180]);
 
-% Differential system of equations
-dif_eq = [eq_th_dth;eq_dw_w];
+% SISTEMA DE ECUACIONES DIFERENCIALES (cinematica, dinamica)
+
+% Separar x de dx
+dif_eq = [eq_th_dth; eq_dw_w];
 eq_splitted = children(dif_eq);
 for i = 1:6
     dif_eq(i) = eq_splitted{i}{1,2};
 end
 
-
-clc
+% Copiar este display en la funcion del final
 disp(dif_eq);
 
 % Solve
 [t,response] = ode45(@Compleate_Equation,[0 60], [th0; w0], odeset('RelTol',1e-4));
-
 for i = 1:6
     h = figure();
     plot(t, response(:,i))
