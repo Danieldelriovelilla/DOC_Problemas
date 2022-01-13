08bd4a233855124275f9871d80f9a7cbaa56ac3a
temisAP <temisap.98@gmail.com>
2022-01-13 13:31:55 +0100

    Update Problems_Lecture_10.m

--- Matlab/Problems_Lecture_10.m
+++ Matlab/Problems_Lecture_10.m
@@ -1,4 +1,51 @@
-%% 
+clc; clear all
+%% Problem 1
+I = 1;    % kg m²
+Mp = 0.2;
+ts = 60;  % s
 
-doc = doc_functions();
-eq = doc.Euler_Equation(rand(3,3),rand(3,1),rand(3,1), rand(3,1))
+syms theta u
+eq = diff(diff(theta)) * I - u ;
+
+% Parámetros deseados
+syms dzeta 
+eq = Mp == exp(-pi*dzeta/(1-dzeta)^0.5);
+[sol] = vpasolve(eq,dzeta);
+
+dzeta = sol.dzeta
+
+%Definir tf genérica
+s = syms2tf(eq);
+
+%Bucle cerrado 
+
+s = s/(1+s);
+
+%Encontrar polos y zeros
+z = zero(s);
+p = pole(s);
+
+pzplot(s)
+grid
+
+%Definir PD
+Kp = 1;
+Ki = 1;
+Kd = 1;
+C = Kp + Kd*s
+
+function[TF] = syms2tf(eq)
+% Get transfer function from equation
+% 
+%   input eq (sym): symbolic equation
+% 
+%   output TF (tf): transfer function
+%
+
+  G = laplace(eq);             %Get Laplace transform of Eq
+  [symNum,symDen] = numden(G); %Get num and den of Symbolic TF
+  TFnum = sym2poly(symNum);    %Convert Symbolic num to polynomial
+  TFden = sym2poly(symDen);    %Convert Symbolic den to polynomial
+  TF =tf(TFnum,TFden);
+  
+end
