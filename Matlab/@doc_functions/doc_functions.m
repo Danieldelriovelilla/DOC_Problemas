295b3b874941aa83d5d8193358665e3e26209e48
temisAP <temisap.98@gmail.com>
2022-01-13 10:34:54 +0100

    Documentation

--- Matlab/@doc_functions/doc_functions.m
+++ Matlab/@doc_functions/doc_functions.m
@@ -1,10 +1,12 @@
 classdef doc_functions < handle
     properties
-        
+
     end
 
     methods (Access = public)
         function obj = doc_functions()
+          disp('Thanks for using our function module!')
+          disp('Go to https://github.com/Danieldelriovelilla/DOC_Problemas -> DOCUMENTATION to get more information ')
 
         end
 
@@ -19,53 +21,53 @@ classdef doc_functions < handle
     disp("C = ")
     disp(C)
     end
-    
+
     function [C] = C1(obj, theta)
     C = [1, 0, 0
         0, cos(theta), sin(theta);
         0, -sin(theta), cos(theta)];
     end
-    
+
     function [C] = C2(obj, theta)
     C = [cos(theta), 0, -sin(theta);
         0, 1, 0
         sin(theta), 0, cos(theta)];
     end
-    
+
     function [C] = C3(obj, theta)
     C = [cos(theta), sin(theta), 0;
         -sin(theta), cos(theta), 0;
         0, 0, 1];
     end
-    
+
     function [C] = C323(obj, t1, t2, t3)
     C = C3(t1)*C2(t2)*C3(t3);
     end
-    
+
     function [C] = C123(obj, t1, t2, t3)
     C = C1(obj, t1)*C2(obj, t2)*C3(obj, t3);
     end
-    
+
     function [theta1] = theta1_321(obj, C)
     theta1 = atan2(C(2,3),C(3,3));
     end
-    
+
     function [theta2] = theta2_321(obj, C)
     theta2 = -asin(C(1,3));
     end
-    
+
     function [theta3] = theta3_321(obj, C)
     theta3 = atan2(C(1,2),C(1,1));
     end
-    
+
     function [C] = C_from_e(obj, phi, e)
     ex = [0, -e(3), e(2);
           e(3), 0, -e(1);
           -e(2), e(1), 0];
     C = cos(phi)*eye(3) + (1-cos(phi))*e'*e - sin(phi)*ex;
     end
-    
-    
+
+
     % Eigenaxis
     function [phi, e] = Eigenaxis(obj, C21)
     phi = acos( ( C21(1,1) + C21(2,2) + C21(3,3) - 1)/2 );
@@ -73,28 +75,28 @@ classdef doc_functions < handle
          C21(3,1) - C21(1,3);
          C21(1,2) - C21(2,1)]/(2*sin(phi));
     end
-    
+
     % Quaternions
     function [q] = Quaternions_from_C(obj, C)
     phi = acos( ( C(1,1) + C(2,2) + C(3,3) - 1)/2 );
     e = [C(2,3) - C(3,2);
          C(3,1) - C(1,3);
          C(1,2) - C(2,1)]/(2*sin(phi));
-    
+
     q = [e(1)*sin(phi/2);
          e(2)*sin(phi/2);
          e(3)*sin(phi/2)];
     q(4) = cos(phi/2);
     end
-    
+
     function [q] = Quaternions_from_e(obj, phi, e)
     q = [e(1)*sin(phi/2);
          e(2)*sin(phi/2);
          e(3)*sin(phi/2)];
     q(4) = cos(phi/2);
     end
-    
-    
+
+
     % Attitude determination
     function [Cb, Ci, Cbi] = Triad_Method(obj, ub, vb, ui, vi)
     % Body
@@ -148,7 +150,7 @@ classdef doc_functions < handle
     function [I] = Inertia_Matrix(obj, I1, I2, I3)
     I = [I1, 0, 0;
          0, I2, 0;
-         0, 0, I3];   
+         0, 0, I3];
     disp("ASEGURATE QUE I1 ES EL MAYOR E I3 EL MENOR")
     end
 
@@ -162,7 +164,7 @@ classdef doc_functions < handle
     function [eq] = Euler_Equation(obj, I, w, dw, T)
     eq = I*dw + cross(w, I*w) == T;
     end
-    
+
     function [T] = Kinetic_Energy(obj, I, w)
     T = w'*I*w;
     end
@@ -176,12 +178,12 @@ classdef doc_functions < handle
     hn = norm(h);
     nutation_angle = asin(ht/hn);
     end
-    
+
     function [precession_rate] = Precession_Rate(obj, I, h)
     hn = norm(h);
     precession_rate = hn/I(1,1);
     end
-    
+
 
 
     end
